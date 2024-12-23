c821b31cc31fb82770826c8765b1d0e3eba12a4b
antonLukshman <159231205+antonLukshman@users.noreply.github.com>
2024-12-23 12:51:41 +0530

    files created

---
+++ src/core/TicketOperation.java
@@ -0,0 +1,6 @@
+package src.core;
+
+    public interface TicketOperation {
+        void addTickets(String ticket);
+        String removeTicket();
+}
---
+++ src/core/TicketPool.java
@@ -0,0 +1,21 @@
+package src.core;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TicketPool implements TicketOperation {
+        private final List<String> tickets = Collections.synchronizedList(new LinkedList<>());
+        @Override
+        public synchronized void addTickets(String ticket) {
+            tickets.add(ticket);
+        }
+        @Override
+        public synchronized String removeTicket() {
+            return tickets.isEmpty() ? null : tickets.remove(0);
+        }
+        public int getTicketCount() {
+            return tickets.size();
+        }
+}
+
